cbf2310eacf9136db7dc2b3cb3d5b56698a2aacc
ˢᵗᵃⁿᵍᵉʳ ˣ <132274315+strangerxmi@users.noreply.github.com>
2023-05-16 10:18:38 +0300

    Delete REDDIT BOT directory

--- REDDIT BOT/auth.py
+++
@@ -1,8 +0,0 @@
-def authenticate():
-    reddit = praw.Reddit(
-            client_id='example',
-            client_secret='example',
-            user_agent='<console:example:1.0>',
-            username='example',
-            password='example'
-        )
